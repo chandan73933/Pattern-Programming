b5aa9655f82b9f508a3b524dcb7f3de511281070
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-06-30 23:55:42 +0530

    Add files via upload

---
+++ newpatt.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,k,l,m,o,p,n,x;
+cin>>n;
+
+for(i=1;i<=n;i++)
+{
+	for(j=n;j>=i;j--)
+	{
+	cout<<"*";
+	}
+	for(k=2;k<=i;k++)
+	{
+	cout<<" ";
+	}
+	for(l=2;l<=i;l++)
+	{
+	cout<<" ";
+	}
+	for(m=n;m>=i;m--)
+	{
+	cout<<"*";
+	}
+	cout<<"\n";
+}
+for(i=1;i<=n;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+	cout<<"*";
+	}
+	for(k=n-i;k>=1;k--)
+	{
+	cout<<" ";
+	}
+	for(l=1;l<=n-i;l++)
+	{
+	cout<<" ";
+	}
+	for(m=1;m<=i;m++)
+	{
+	cout<<"*";
+	}
+	cout<<"\n";
+}
+}
